4447e5fc55f1fb53a340e93117f12a12b23a3682
baptistebellencontre <baptiste.bellencontre@edu.ece.fr>
2024-11-19 14:12:21 +0100

    Update display.c

--- display.c
+++ display.c
@@ -2,3 +2,8 @@
 #include "player.h"
 #include "display.h"
 #include <stdio.h>
+
+void set_text_color(int color) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}
