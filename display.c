1f47407a896607ac55dfdd95b68302c41e4ccfa8
baptistebellencontre <baptiste.bellencontre@edu.ece.fr>
2024-11-19 14:12:39 +0100

    Update display.c

--- display.c
+++ display.c
@@ -7,3 +7,10 @@ void set_text_color(int color) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, color);
 }
+
+void display_colored_token(char token, int color) {
+    set_text_color(color);
+    printf("%c", token);
+    set_text_color(15); 
+}
+
