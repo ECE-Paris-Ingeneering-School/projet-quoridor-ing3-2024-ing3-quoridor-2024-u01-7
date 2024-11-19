5daa549dca299dcbfa0ad3fd2eeaea330632f4ee
baptistebellencontre <baptiste.bellencontre@edu.ece.fr>
2024-11-19 14:04:45 +0100

    Update game.c

--- game.c
+++ game.c
@@ -4,3 +4,10 @@
 #include "save.h"
 #include <stdio.h>
 #include <windows.h>
+
+void gotoligcol(int lig, int col) {
+    COORD mycoord;
+    mycoord.X = col;
+    mycoord.Y = lig;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), mycoord);
+}
