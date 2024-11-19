c90d44980924e690de3a2bb4cd7d34658e82e26c
baptistebellencontre <baptiste.bellencontre@edu.ece.fr>
2024-11-19 14:04:03 +0100

    Update save.c

--- save.c
+++ save.c
@@ -23,3 +23,27 @@ void save_game(Game *game) {
     fclose(f);
     printf("Partie sauvegardée avec succès.\n");
 }
+
+int load_game(Game *game) {
+    FILE *f = fopen("sauvegarde.txt", "r");
+    if (f == NULL) {
+        printf("Aucune partie sauvegardée trouvée.\n");
+        return 0;
+    }
+
+    fscanf(f, "%d\n", &game->num_players);
+    for (int i = 0; i < game->num_players; i++) {
+        fscanf(f, "%s %d %d %d %d\n", game->players[i].name, &game->players[i].x, &game->players[i].y, &game->players[i].barriers, &game->players[i].is_human);
+        game->players[i].symbol = '1' + i;
+    }
+
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            fscanf(f, " %c", &game->board[i][j]);
+        }
+    }
+
+    fclose(f);
+    printf("Partie chargée avec succès.\n");
+    return 1;
+}
