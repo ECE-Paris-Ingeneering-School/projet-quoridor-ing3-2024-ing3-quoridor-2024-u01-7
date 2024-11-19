c4b84e05b7ba76b26208a9d773c1b016f77fdbdf
baptistebellencontre <baptiste.bellencontre@edu.ece.fr>
2024-11-19 14:06:45 +0100

    Update player.c

--- player.c
+++ player.c
@@ -15,15 +15,13 @@ bool is_symbol_unique(Player *players, int num_players, char symbol) {
 
 void setup_players(Player players[], int *num_players, char board[BOARD_SIZE][BOARD_SIZE]) {
     for (int i = 0; i < *num_players; i++) {
-        // Nom du joueur
         printf("Entrez le nom du joueur %d : ", i + 1);
         scanf("%s", players[i].name);
 
-        // Symbole du joueur
+        
         do {
             printf("Choisissez un symbole pour le joueur %s (par exemple @, #, $, &) : ", players[i].name);
             scanf(" %c", &players[i].symbol);
-            // Vérification que le symbole n'est pas déjà utilisé
             int symbol_used = 0;
             for (int j = 0; j < i; j++) {
                 if (players[j].symbol == players[i].symbol) {
@@ -35,28 +33,25 @@ void setup_players(Player players[], int *num_players, char board[BOARD_SIZE][BO
             if (!symbol_used) break;
         } while (1);
 
-        // Type de joueur (Humain ou IA)
         do {
             printf("Le joueur %s est-il un humain (1) ou une IA (0) ? ", players[i].name);
             scanf("%d", &players[i].is_human);
         } while (players[i].is_human != 0 && players[i].is_human != 1);
 
-        // Positionnement initial des joueurs
         if (i == 0) {
-            players[i].x = 0;           // En haut
+            players[i].x = 0;           
             players[i].y = BOARD_SIZE / 2;
         } else if (i == 1) {
-            players[i].x = BOARD_SIZE - 1; // En bas
+            players[i].x = BOARD_SIZE - 1; 
             players[i].y = BOARD_SIZE / 2;
         } else if (i == 2) {
-            players[i].x = BOARD_SIZE / 2; // À gauche
+            players[i].x = BOARD_SIZE / 2; 
             players[i].y = 0;
         } else if (i == 3) {
-            players[i].x = BOARD_SIZE / 2; // À droite
+            players[i].x = BOARD_SIZE / 2; 
             players[i].y = BOARD_SIZE - 1;
         }
 
-        // Placement du pion sur le plateau
         board[players[i].x][players[i].y] = players[i].symbol;
     }
 }
