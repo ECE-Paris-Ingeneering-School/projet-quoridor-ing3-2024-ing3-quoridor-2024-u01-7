66306925d0b85f999acdbdd5cf34791ea1ebaea1
baptistebellencontre <baptiste.bellencontre@edu.ece.fr>
2024-11-19 14:02:16 +0100

    Update player.c

--- player.c
+++ player.c
@@ -3,3 +3,12 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
+
+bool is_symbol_unique(Player *players, int num_players, char symbol) {
+    for (int i = 0; i < num_players; i++) {
+        if (players[i].symbol == symbol) {
+            return false;
+        }
+    }
+    return true;
+}
