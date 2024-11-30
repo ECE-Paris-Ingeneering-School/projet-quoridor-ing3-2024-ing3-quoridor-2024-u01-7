185da315d8ed47164ae0282eadfb8ecebe618c66
baptistebellencontre <baptiste.bellencontre@edu.ece.fr>
2024-11-30 17:10:19 +0100

    Update barrieres.h

--- barrieres.h
+++ barrieres.h
@@ -3,7 +3,6 @@
 
 #include "structures.h"
 
-// Fonction pour placer une barrière
 int placerBarriere(char plateau[TAILLE_PLATEAU][TAILLE_PLATEAU], char case1[], char case2[], char position);
 
 #endif
