fc95c329e2b46a7f2c995deeb5d9ca8e8804e82a
baptistebellencontre <baptiste.bellencontre@edu.ece.fr>
2024-11-30 17:17:31 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,14 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "menu.h"
 
+#include "jeu.h"
+
 void afficherMenu() {
-    printf("\n--- Menu Principal ---\n");
-    printf("1. Nouvelle Partie\n");
-    printf("2. Reprendre Partie\n");
-    printf("3. Afficher Aide\n");
-    printf("4. Afficher Scores\n");
-    printf("5. Quitter\n");
+    int choix;
+    do {
+        printf("\n--- Menu Principal ---\n");
+        printf("1. Nouvelle Partie\n");
+        printf("2. Reprendre Partie\n");
+        printf("3. Afficher Aide\n");
+        printf("4. Sauvegarder Partie\n");
+        printf("5. Quitter\n");
+        printf("Votre choix : ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1:
+                nouvellePartie(); 
+                break;
+            case 2:
+                printf("Reprendre partie : fonctionnalité en cours de développement.\n");
+                break;
+            case 3:
+                afficherAide();
+                break;
+            case 4:
+                printf("Sauvegarder partie : fonctionnalité en cours de développement.\n");
+                break;
+            case 5:
+                printf("Merci d'avoir joué ! À bientôt !\n");
+                exit(0);
+            default:
+                printf("Choix invalide. Réessayez.\n");
+        }
+    } while (choix != 5);
 }
+
 
 void afficherAide() {
     printf("\n--- Aide ---\n");
