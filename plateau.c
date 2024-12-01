57a951341c5b7dae47d0abe2cf1c9179be918c33
baptistebellencontre <baptiste.bellencontre@edu.ece.fr>
2024-12-01 18:32:12 +0100

    Update plateau.c

--- plateau.c
+++ plateau.c
@@ -6,7 +6,7 @@
 void initialiserPlateau(char plateau[TAILLE_PLATEAU][TAILLE_PLATEAU]) {
     for (int i = 0; i < TAILLE_PLATEAU; i++) {
         for (int j = 0; j < TAILLE_PLATEAU; j++) {
-            plateau[i][j] = '.'; 
+            plateau[i][j] = '.'; // Case vide
         }
     }
 }
@@ -38,10 +38,70 @@ void afficherPlateau(char plateau[TAILLE_PLATEAU][TAILLE_PLATEAU], Joueur joueur
 }
 
 void placerJoueursSurPlateau(Joueur joueurs[], int nbJoueurs, char plateau[TAILLE_PLATEAU][TAILLE_PLATEAU]) {
-    for (int i = 0; i < nbJoueurs; i++) {
-        plateau[joueurs[i].x][joueurs[i].y] = joueurs[i].symbole;
+    // Réinitialiser le plateau avec des cases vides
+    for (int i = 0; i < TAILLE_PLATEAU; i++) {
+        for (int j = 0; j < TAILLE_PLATEAU; j++) {
+            plateau[i][j] = '.'; // Case vide
+        }
+    }
+
+    // Placer les joueurs en fonction du nombre de joueurs
+    if (nbJoueurs == 2) {
+        // Joueur 1 commence en bas-centre
+        joueurs[0].x = TAILLE_PLATEAU - 1;
+        joueurs[0].y = TAILLE_PLATEAU / 2;
+        joueurs[0].directionCible = 'H'; // Objectif : haut
+        joueurs[0].ligneCible = 0;
+
+        // Joueur 2 commence en haut-centre
+        joueurs[1].x = 0;
+        joueurs[1].y = TAILLE_PLATEAU / 2;
+        joueurs[1].directionCible = 'B'; // Objectif : bas
+        joueurs[1].ligneCible = TAILLE_PLATEAU - 1;
+
+        plateau[joueurs[0].x][joueurs[0].y] = joueurs[0].symbole;
+        plateau[joueurs[1].x][joueurs[1].y] = joueurs[1].symbole;
+
+        printf("Débogage : Joueur 1 (%c) placé en (%d, %d).\n", joueurs[0].symbole, joueurs[0].x, joueurs[0].y);
+        printf("Débogage : Joueur 2 (%c) placé en (%d, %d).\n", joueurs[1].symbole, joueurs[1].x, joueurs[1].y);
+
+    } else if (nbJoueurs == 4) {
+        // Joueur 1 commence en bas-centre
+        joueurs[0].x = TAILLE_PLATEAU - 1;
+        joueurs[0].y = TAILLE_PLATEAU / 2;
+        joueurs[0].directionCible = 'H'; // Objectif : haut
+        joueurs[0].ligneCible = 0;
+
+        // Joueur 2 commence en haut-centre
+        joueurs[1].x = 0;
+        joueurs[1].y = TAILLE_PLATEAU / 2;
+        joueurs[1].directionCible = 'B'; // Objectif : bas
+        joueurs[1].ligneCible = TAILLE_PLATEAU - 1;
+
+        // Joueur 3 commence en milieu-gauche
+        joueurs[2].x = TAILLE_PLATEAU / 2;
+        joueurs[2].y = 0;
+        joueurs[2].directionCible = 'D'; // Objectif : droite
+        joueurs[2].ligneCible = TAILLE_PLATEAU - 1;
+
+        // Joueur 4 commence en milieu-droit
+        joueurs[3].x = TAILLE_PLATEAU / 2;
+        joueurs[3].y = TAILLE_PLATEAU - 1;
+        joueurs[3].directionCible = 'G'; // Objectif : gauche
+        joueurs[3].ligneCible = 0;
+
+        for (int i = 0; i < nbJoueurs; i++) {
+            plateau[joueurs[i].x][joueurs[i].y] = joueurs[i].symbole;
+            printf("Débogage : Joueur %d (%c) placé en (%d, %d).\n", i + 1, joueurs[i].symbole, joueurs[i].x, joueurs[i].y);
+        }
+    } else {
+        printf("Erreur : Nombre de joueurs non supporté.\n");
+        exit(1); // Terminer le programme
     }
 }
+
+
+
 
 void afficherInfosJoueurs(Joueur joueurs[], int nbJoueurs) {
     printf("\n--- Informations des joueurs ---\n");
